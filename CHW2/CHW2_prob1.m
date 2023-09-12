1a7ddd2268c5a044967746c727341a13e5712ba0
jkwoody1 <129899304+jkwoody1@users.noreply.github.com>
2023-09-12 13:00:12 -0500

    Commiting CHW2 problem 1's question

---
+++ CHW2/CHW2_prob1.m
@@ -0,0 +1,7 @@
+% Problem 1 
+
+%Evaluate the function
+%f(x) = (1 − ((1 - x)^3))/ x
+% for x = 10^(-1), ..., 10^(−14). Then, using an alternative form of expression for f(x)
+%that avoids significance loss, repeat the same calculation and make a table of
+%all the results obtained.
