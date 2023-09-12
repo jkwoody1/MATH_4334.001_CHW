38678ef28e275f23cc8de8ac0a1b578725356fb3
jkwoody1 <129899304+jkwoody1@users.noreply.github.com>
2023-09-12 13:32:13 -0500

    Selective answer to prob 2

--- CHW2/CHW2_prob2.m
+++ CHW2/CHW2_prob2.m
@@ -3,3 +3,12 @@
 % Consider a right triangle whose legs are of length 3344556600 and 1.2222222.
 % How much longer is the hypotenuse than the longer leg? Give your answer with
 % at least four correct digits.
+
+a = 3344556600;
+b = 1.2222222; 
+
+c = sqrt( (a^2)  + (b^2));
+
+
+% Is it correct? 
+display(c)
