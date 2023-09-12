fbb1de8e467266b6026962021606e478673ca7d1
jkwoody1 <129899304+jkwoody1@users.noreply.github.com>
2023-09-12 13:01:48 -0500

    CHW 2 problem 2 question setup

---
+++ CHW2/CHW2_prob2.m
@@ -0,0 +1,5 @@
+% Problem 2 
+
+% Consider a right triangle whose legs are of length 3344556600 and 1.2222222.
+% How much longer is the hypotenuse than the longer leg? Give your answer with
+% at least four correct digits.
