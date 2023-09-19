192e2a29304c6cb5beec83c25c61297099c66ccd
jkwoody1 <129899304+jkwoody1@users.noreply.github.com>
2023-09-19 11:41:51 -0500

    Basic Changes

--- CHW2/CHW2_prob2.m
+++ CHW2/CHW2_prob2.m
@@ -1,5 +1,3 @@
-% Problem 2 
-
 % Consider a right triangle whose legs are of length 3344556600 and 1.2222222.
 % How much longer is the hypotenuse than the longer leg? Give your answer with
 % at least four correct digits.
@@ -13,10 +11,18 @@ end
 
 A % answer for the table
 
-a=3344556600; b=1.2222222;% ginven values for the legs of a right triangle
+
+% Problem 2
+
+a=3344556600; b=1.2222222;
+
+% ginven values for the legs of a right triangle
 % hypotenuse of a right triangle = sqrt(a^2 + b^2) (by Phythagorean thm)
 % difference = hypotenuse - the longer leg(a)= sqrt(a^2 + b^2) -a
 % since b is very small, sqrt(a^2 + b^2) is very close to a
 % so, difference has a potential loss of significance
 % need to use alternative form using rationalizing
-difference=b^2/(sqrt(a^2 +b^2)+a) % equivalent to "sqrt(a^2 + b^2) -a"
+
+difference=b^2/(sqrt(a^2 +b^2)+a) 
+
+% equivalent to "sqrt(a^2 + b^2) -a"
