6ea8d364f1420a263055ce666780329b8bf63562
jkwoody1 <129899304+jkwoody1@users.noreply.github.com>
2023-09-27 22:09:44 -0500

    small update

--- CHW3/CHW3_Prob1.m
+++ CHW3/CHW3_Prob1.m
@@ -7,13 +7,13 @@ plot(x,y,'r')
 
 %1-2
 syms x
-f(x) = (2*(x^3))- (6*x) -1; % the given function
+f = @(x) (2*(x^3))- (6*x) -1; % the given function
 % using the Intermediate-Value theorem
 % to check the intervals that contain a zero
 
 for i=-2:2
 if f(i)*f(i+1)<0 %checking the signs at both end points in interval of length one
-interval=[i,i+1] % print the inverval with opposite signs at end points
+interval = [i,i+1]; % print the inverval with opposite signs at end points
 a=i; b=i+1;
 while (b-a)/2>10^-3
 c = (a + b)/2;
