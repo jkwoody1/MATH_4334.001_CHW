9787a4995ef69bf1ab61c514b91e900abe6aa1f1
racmmm <145486715+racmmm@users.noreply.github.com>
2023-09-29 21:36:36 -0500

    Update CHW3_Prob3.m

--- CHW3/CHW3_Prob3.m
+++ CHW3/CHW3_Prob3.m
@@ -1,17 +1,17 @@
 % Define the function 
 
-f = @(x) exp(x) + x - 7; 
+f = @(x) exp(x) + x - 7; % The given function
 
-% Initial Guesses
+% Initial Guesses  
 
 x0 = 1; 
 x1 = 2; 
 
 % Set Tolerance 
-toelrance = .00000001;
+tolerance = .00000001;  
 
 
-% Secant Method Iterations
+% Secant Method Iterations 
 iterations = 0; 
 while abs(x1 - x0) >= tolerance
     x2 = x1 - (f(x1) * (x1 - x0)) / (f(x1) - f(x0));
@@ -23,4 +23,5 @@ end
 
 % Result
 
-fprintf('Approximate solution after %d iterations: %.8f\n', iteration, x2); 
+fprintf('Approximate solution after %d iterations: %.8f\n', iteration, x2);  
+% 3 iterations using the second method
